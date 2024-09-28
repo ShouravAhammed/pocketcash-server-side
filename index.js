8b509c64ceb392f6b3c393f73584a41d728c0403
ShouravAhammed <shouravahammed.1@gmail.com>
2024-09-28 23:18:03 +0600

    bug fix cashOut request

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ const corsOptions = {
     });
   };
 
-  console.log(process.env.DB_USER)
-  console.log(process.env.DB_PASS)
+  // console.log(process.env.DB_USER)
+  // console.log(process.env.DB_PASS)
 
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgv1zq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   
